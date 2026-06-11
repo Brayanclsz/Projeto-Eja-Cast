10b8a01c4e84b981f9b2aa7afc1eaa681bb51b8f
Matheus Feitosa de Freitas <matheus.feitosa@pge.pe.gov.br>
2026-06-11 17:29:26 -0300

    ajustando cabeçalho

--- obj/Debug/net8.0/ProjetoEjaCast.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjetoEjaCast.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoEjaCast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b38c72a7144a794654715c976a13502e2eac91b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2bf9ab69d361d17fddddd71f0914df6dd3eabd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoEjaCast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoEjaCast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
